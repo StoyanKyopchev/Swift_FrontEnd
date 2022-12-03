37887f73e844b7779bd6c9db3c075bc216b947a4
StoyanKyopchev <115925638+StoyanKyopchev@users.noreply.github.com>
2022-12-03 10:33:53 +0200

    Update Script.js

--- Lecture 15 Homework - 27.11.2022/Task 1/Script.js
+++ Lecture 15 Homework - 27.11.2022/Task 1/Script.js
@@ -3,12 +3,12 @@ var isButtonClicked = false;
 function onButtonClick() {
 
     if(isButtonClicked) {
-        document.querySelector(".tooltipItem > .plus").innerHTML = "+";
-        document.querySelector(".tooltipItem > .tooltipWrapper").setAttribute("style", "display: none");
+        document.querySelector(".plus").innerHTML = "+";
+        document.querySelector(".tooltipWrapper").setAttribute("style", "display: none");
         isButtonClicked = false;
     } else {
-        document.querySelector(".tooltipItem > .plus").innerHTML = "-";
-        document.querySelector(".tooltipItem > .tooltipWrapper").setAttribute("style", "display: block");
+        document.querySelector(".plus").innerHTML = "-";
+        document.querySelector(".tooltipWrapper").setAttribute("style", "display: block");
         isButtonClicked = true;
     }
 }
